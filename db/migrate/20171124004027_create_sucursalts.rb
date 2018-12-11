8fb7891c041cd08907b0cc4e725d79eb0520348e
ediliodev <edilio7@hotmail.com>
2018-12-10 21:24:55 -0400

    update no. ted10

--- db/migrate/20171124004027_create_sucursalts.rb
+++ db/migrate/20171124004027_create_sucursalts.rb
@@ -3,8 +3,13 @@ class CreateSucursalts < ActiveRecord::Migration[5.0]
     create_table :sucursalts do |t|
       t.string :nombre
       t.string :descripcion
+      t.references :consorciot, foreign_key: true
 
       t.timestamps
     end
   end
 end
+
+
+#20181210063549_create_sucursalts
+#20171124004028_create_empleadots
