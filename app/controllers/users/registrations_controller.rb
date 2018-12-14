d730cf74456fbdcb94be7db1696fe6a8cf0288ad
ediliodev <edilio7@hotmail.com>
2018-12-13 23:13:45 -0400

    Act edit/create devise fix ok2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
 before_action :configure_account_update_params, only: [:update]
 before_action :configure_sign_up_params  # sign_up_params
-before_action :authenticate_user!, :redirect_unless_admin,  only: [:new, :create]
+before_action :authenticate_user!, :redirect_unless_admin #,  only: [:new, :create] (En todos)
 skip_before_action :require_no_authentication
 
   # GET /resource/sign_up
