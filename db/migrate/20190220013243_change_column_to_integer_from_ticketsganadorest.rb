6648a1686d52fcdf55eaaaac18b4d22ac3b8363e
ediliodev <edilio7@hotmail.com>
2019-02-19 22:13:25 -0400

    actualizar column type string to integer en Ticketsganadorests ok posgresql varchar to integer sum test02

--- db/migrate/20190220013243_change_column_to_integer_from_ticketsganadorest.rb
+++ db/migrate/20190220013243_change_column_to_integer_from_ticketsganadorest.rb
@@ -1,6 +1,6 @@
-class ChangeColumnToIntegerFromTicketsganadorest < ActiveRecord::Migration[5.0]
-  def change
-	change_column(:ticketsganadorests, :montoapostado, :integer)
-	change_column(:ticketsganadorests, :montoacobrar, :integer)
-  end
-end
+#class ChangeColumnToIntegerFromTicketsganadorest < ActiveRecord::Migration[5.0]
+#  def change
+#	change_column(:ticketsganadorests, :montoapostado, :integer)
+#	change_column(:ticketsganadorests, :montoacobrar, :integer)
+#  end
+#end
