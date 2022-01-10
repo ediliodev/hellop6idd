4858ead61adfd548d5ac301fef3abb6991833c7a
ediliodev <edilio7@hotmail.com>
2022-01-10 05:32:58 -0400

    modicifaciones migracion provinciat

--- db/migrate/20171124004739_create_provinciats.rb
+++ db/migrate/20171124004739_create_provinciats.rb
@@ -3,12 +3,8 @@ class CreateProvinciats < ActiveRecord::Migration[5.0]
     create_table :provinciats do |t|
       t.string :nombre
       t.references :paist, foreign_key: true
+
       t.timestamps
     end
   end
 end
-
-
-# 20181005052603_create_provinciats.rb
-# 20181005050714_create_paists.rb
-#20181005050715
